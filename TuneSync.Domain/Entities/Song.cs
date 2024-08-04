53b2f6e295cf7cbc45e2f4aaf39963f11dfc967e
artur10563 <galkaartur2004galkaartur@gmail.com>
2024-08-04 20:23:46 +0300

    Firestore; TODO: FIX DOMAIN
    Firestore; TODO: FIX DOMAIN

---
+++ TuneSync.Domain/Entities/Song.cs
@@ -0,0 +1,20 @@
+﻿using Google.Cloud.Firestore; //TODO: Clean arch violation
+
+namespace TuneSync.Domain.Entities
+{
+	[FirestoreData]
+	public class Song
+	{
+		[FirestoreProperty]
+		public int Id { get; set; }
+
+		[FirestoreProperty]
+		public string Name { get; set; }
+
+		[FirestoreProperty]
+		public string VideoUrl { get; set; }
+		
+		[FirestoreProperty]
+		public string AudioUrl { get; set; }
+	}
+}
