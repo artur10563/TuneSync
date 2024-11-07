31a5ceba0874e122a37b228de7161c3fb6e9c2c6
artur10563 <galkaartur2004galkaartur@gmail.com>
2024-11-07 17:30:24 +0200

    JWT auth

---
+++ Api/Endpoints/AuthEndpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace Api.Endpoints
+{
+    public static class AuthEndpoints
+    {
+        public static async Task RegisterAuthEndpoints(this IEndpointRouteBuilder app)
+        {
+            var auth = app.MapGroup("api/auth");
+
+            auth.MapPost("/register", () => { });
+            auth.MapGet("/login", () => { });
+        }
+    }
+}
---
+++ Infrastructure/Services/JwtService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class JwtService
+    {
+        public string GenerateToken(Guid userGuid)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = "getThisFromConfiguration"u8.ToArray();
+
+            var claims = new List<Claim>()
+            {
+                new (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // Unique Guid for current Token
+                new (JwtRegisteredClaimNames.Sub, userGuid.ToString())
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(60),
+                Issuer = "https://testIssuer.com",
+                Audience = "http://localhost:4200/",
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
