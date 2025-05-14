347ca55f039eb8407d0a8e88dc2f59c385059b5b
artur10563 <galkaartur2004galkaartur@gmail.com>
2025-05-14 20:46:52 +0300

    Playlist deletion fix

--- Infrastructure/Repositories/PlaylistRepository.cs
+++ Infrastructure/Repositories/PlaylistRepository.cs
@@ -15,7 +15,7 @@ namespace Infrastructure.Repositories
 
         public override Task<Playlist?> FirstOrDefaultWithDependantAsync(Expression<Func<Playlist, bool>> predicate)
         {
-            return _set.Include(pl => pl.Songs).FirstOrDefaultAsync();
+            return _set.Include(pl => pl.Songs).FirstOrDefaultAsync(predicate);
         }
     }
 }
